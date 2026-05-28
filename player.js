fcc82372c965a322b3aa934027bf25d06c8d7198
tokoblotongan <blotonganjayalengkap@gmail.com>
2026-05-28 10:13:48 +0700

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,33 @@
+let currentCharacter = "bimbim";
 
+const player = {
+    x: 100,
+    y: 300,
+    width: 30,
+    height: 50,
+    speed: 4,
+    velocityX: 0,
+    velocityY: 0,
+    jumping: false,
+    grounded: false,
+    isDead: false,
+    deathTimer: 0
+};
+
+function drawPlayer(ctx) {
+    if (currentCharacter === "bimbim") {
+        // Visual Bimbim
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(player.x, player.y, player.width, player.height);
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(player.x + 5, player.y + 5, player.width - 10, 10);
+    } else if (currentCharacter === "chacha") {
+        // Visual Chacha
+        ctx.fillStyle = "#ffb6c1";
+        ctx.fillRect(player.x, player.y, player.width, player.height);
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(player.x, player.y + 35, player.width, 15);
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(player.x - 2, player.y, player.width + 4, 12);
+    }
+}
