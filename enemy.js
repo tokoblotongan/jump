5b7fbec95bd99c260710d88956e1e19c0d58c6a8
tokoblotongan <blotonganjayalengkap@gmail.com>
2026-05-28 10:15:34 +0700

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,1 +1,19 @@
+const enemy = {
+    x: 700,
+    y: 320,
+    width: 30,
+    height: 30,
+    color: "#8B4513",
+    speed: 1.5,
+    velocityX: 0,
+    velocityY: 0,
+    alive: true,
+    isDefeated: false
+};
 
+function drawEnemy(ctx) {
+    if (enemy.alive || enemy.isDefeated) {
+        ctx.fillStyle = enemy.color;
+        ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+    }
+}
