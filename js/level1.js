5168728e8bd2a2eaf66f439e88b6f953124298cf
tokoblotongan <blotonganjayalengkap@gmail.com>
2026-06-03 14:25:55 +0700

    Refactor platform and enemy definitions in level1.js

--- js/level1.js
+++ js/level1.js
@@ -4,43 +4,78 @@ const level1 = {
 
     platforms:[
 
-        // Rumah
+        // START - Rumah Bimbim
         {x:0,y:350,w:1000,h:400},
 
-        // Batu pijakan selokan
+        // Selokan 1
+        // Buku tulis sebagai pijakan
         {x:1080,y:300,w:120,h:50},
 
-        // Gang warga
+        // Jalan menuju gang
         {x:1300,y:350,w:900,h:400},
 
-        // Batu pijakan kedua
-        {x:2280,y:280,w:120,h:70},
+        // Selokan 2 (lebih sulit)
+
+        // Buku
+        {x:2280,y:300,w:120,h:50},
+
+        // Penggaris
+        {x:2500,y:250,w:180,h:30},
+
+        // Kotak pensil
+        {x:2750,y:280,w:120,h:50},
 
         // Area jemuran
-        {x:2500,y:350,w:900,h:400},
+        {x:3000,y:350,w:900,h:400},
 
-        // Area pos ronda
-        {x:3800,y:350,w:1200,h:400}
+        // Selokan 3 (terpanjang)
+
+        // Buku
+        {x:3980,y:300,w:120,h:50},
+
+        // Penggaris
+        {x:4200,y:240,w:180,h:30},
+
+        // Buku
+        {x:4470,y:280,w:120,h:50},
+
+        // Area sekolah
+        {x:4700,y:350,w:1200,h:400}
     ],
 
     enemies:[
+
+        // Ular pertama
         {x:1600,y:290},
-        {x:2800,y:290},
-        {x:4100,y:290}
+
+        // Ular kedua
+        {x:3300,y:290},
+
+        // Ular ketiga
+        {x:5000,y:290}
     ],
 
     items:[
+
+        // Buku tulis
         {type:"notebook",x:300,y:280},
-        {type:"milk",x:2600,y:280},
-        {type:"ruler",x:3900,y:280}
+
+        // Susu
+        {type:"milk",x:1850,y:280},
+
+        // Penggaris
+        {type:"ruler",x:3400,y:280},
+
+        // Pensil
+        {type:"pencil",x:5200,y:280}
     ],
 
     checkpoint:{
-        x:3600
+        x:3900
     },
 
     boss:{
-        x:4500,
+        x:5500,
         hp:10
     }
 };
